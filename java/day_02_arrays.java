161e3d18784900579beb03af1ad234e5c77c1a17
anandeekhare <khareanandee11@gmail.com>
2026-02-13 12:24:35 +0530

    day 2: practiced basic arrays in java

---
+++ java/day_02_arrays.java
@@ -0,0 +1,8 @@
+public class Day02 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
