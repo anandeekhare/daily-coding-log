3abc56075eeb28c4d785f32f71dc30a399342927
anandeekhare <khareanandee11@gmail.com>
2026-02-15 01:06:18 +0530

    day 4: stack basics practice

---
+++ leetcode/day_04_stack.py
@@ -0,0 +1,11 @@
+# stack basics
+
+stack = []
+
+stack.append(5)
+stack.append(10)
+stack.append(15)
+
+print("Top element:", stack[-1])
+print("After pop:", stack.pop())
+print("Stack now:", stack)
