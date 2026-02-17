ec1ebf31d49551165bacd5af387ed007842b96c5
anandeekhare <khareanandee11@gmail.com>
2026-02-17 21:58:59 +0530

    day 6: practiced string basics in java

---
+++ java/day_06_strings.java
@@ -0,0 +1,10 @@
+public class Day06 {
+    public static void main(String[] args) {
+        String name = "Anandee";
+
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+        System.out.println("First char: " + name.charAt(0));
+    }
+}
