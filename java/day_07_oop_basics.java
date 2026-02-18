427d971d9665b1164d26fe64ce3c96a581eaea67
anandeekhare <khareanandee11@gmail.com>
2026-02-18 19:51:08 +0530

    added constructor in student class

--- java/day_07_oop_basics.java
+++ java/day_07_oop_basics.java
@@ -2,10 +2,18 @@ class Student {
     String name;
     int age;
 
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
     void introduce() {
         System.out.println("Hi, I am " + name + " and I am " + age + " years old.");
     }
 }
+
+Student s1 = new Student("Anandee", 18);
+s1.introduce();
 
 public class Day07 {
     public static void main(String[] args) {
