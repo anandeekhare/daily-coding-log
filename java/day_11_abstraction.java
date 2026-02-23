adaf304df01d5657f8eb2c1edcf5e80a55670bd5
anandeekhare <khareanandee11@gmail.com>
2026-02-23 09:27:10 +0530

    day 11: practiced abstraction in java

---
+++ java/day_11_abstraction.java
@@ -0,0 +1,21 @@
+abstract class Animal {
+    abstract void sound();
+
+    void sleep() {
+        System.out.println("Animal sleeps");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Day11 {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        d.sleep();
+    }
+}
