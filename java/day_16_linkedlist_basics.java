aba8cd8504dee23cccd5dfcb50f7a8678ed12df1
anandeekhare <khareanandee11@gmail.com>
2026-02-28 23:14:06 +0530

    day 16: created and traversed singly linked list

---
+++ java/day_16_linkedlist_basics.java
@@ -0,0 +1,23 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Day16 {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
