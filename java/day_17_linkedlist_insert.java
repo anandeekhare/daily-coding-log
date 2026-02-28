d1f5131b8af4800a587e6a7d5bdc100ce74266d3
anandeekhare <khareanandee11@gmail.com>
2026-03-01 05:26:15 +0530

    day 17: insertion in linked list (start and end)

---
+++ java/day_17_linkedlist_insert.java
@@ -0,0 +1,50 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Day17 {
+
+    // insert at beginning
+    static Node insertAtStart(Node head, int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+        return newNode;
+    }
+
+    // insert at end
+    static Node insertAtEnd(Node head, int val) {
+        Node newNode = new Node(val);
+
+        if (head == null) return newNode;
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
+
+    // print list
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(20);
+        head = insertAtStart(head, 10);
+        head = insertAtEnd(head, 30);
+
+        printList(head);
+    }
+}
