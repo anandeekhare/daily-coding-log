c9c6ac856bb003ad2034e489264a92b1ab93e045
anandeekhare <khareanandee11@gmail.com>
2026-03-03 22:41:26 +0530

    day 19: reversed singly linked list

---
+++ java/day_19_reverse_linkedlist.java
@@ -0,0 +1,42 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Day19 {
+
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        head = reverse(head);
+        printList(head);
+    }
+}
