a78c41f15c1f81f072f3344c7846f5e175fafb33
anandeekhare <khareanandee11@gmail.com>
2026-03-04 22:27:49 +0530

    day 21: find middle of linked list using fast slow pointer

---
+++ java/day_21_middle_linkedlist.java
@@ -0,0 +1,44 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Day21 {
+
+    static Node findMiddle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        Node mid = findMiddle(head);
+        System.out.println("Middle: " + mid.data);
+    }
+}
