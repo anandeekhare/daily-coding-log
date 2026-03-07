fb5e3f914d92956b92c2e8d2cf943555ed95374b
anandeekhare <khareanandee11@gmail.com>
2026-03-07 21:54:17 +0530

    day 23: merged two sorted linked lists

---
+++ java/day_23_merge_sorted_lists.java
@@ -0,0 +1,56 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Day23 {
+
+    static Node merge(Node l1, Node l2) {
+        Node dummy = new Node(0);
+        Node tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.data < l2.data) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (l1 != null) tail.next = l1;
+        if (l2 != null) tail.next = l2;
+
+        return dummy.next;
+    }
+
+    static void printList(Node head) {
+        Node t = head;
+        while (t != null) {
+            System.out.print(t.data + " ");
+            t = t.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node l1 = new Node(1);
+        l1.next = new Node(3);
+        l1.next.next = new Node(5);
+
+        Node l2 = new Node(2);
+        l2.next = new Node(4);
+        l2.next.next = new Node(6);
+
+        Node result = merge(l1, l2);
+
+        printList(result);
+    }
+}
