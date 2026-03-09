463cba59b165bd3e5fb4302a84b985dd4c62a2e6
Anandee Khare <khareanandee11@gmail.com>
2026-03-09 21:31:56 +0530

    day 25: intersection of two linked lists using two pointers

---
+++ java/day_25_intersection_linkedlist.java
@@ -0,0 +1,46 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Day25 {
+
+    static Node getIntersection(Node headA, Node headB) {
+
+        Node a = headA;
+        Node b = headB;
+
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        Node common = new Node(8);
+        common.next = new Node(10);
+
+        Node headA = new Node(3);
+        headA.next = new Node(7);
+        headA.next.next = common;
+
+        Node headB = new Node(99);
+        headB.next = new Node(1);
+        headB.next.next = common;
+
+        Node intersection = getIntersection(headA, headB);
+
+        if (intersection != null)
+            System.out.println("Intersection at: " + intersection.data);
+        else
+            System.out.println("No intersection");
+    }
+}
