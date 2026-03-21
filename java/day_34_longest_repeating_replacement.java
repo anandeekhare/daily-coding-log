4c07b2fcb8661f44feea62ab806bc239b91d0c5a
Anandee Khare <khareanandee11@gmail.com>
2026-03-21 13:07:10 +0530

    day 34: longest repeating character replacement using sliding window

---
+++ java/day_34_longest_repeating_replacement.java
@@ -0,0 +1,31 @@
+public class Day34 {
+
+    static int characterReplacement(String s, int k) {
+
+        int[] freq = new int[26];
+        int left = 0, maxCount = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            freq[s.charAt(right) - 'A']++;
+            maxCount = Math.max(maxCount, freq[s.charAt(right) - 'A']);
+
+            while ((right - left + 1) - maxCount > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "AABABBA";
+        int k = 1;
+
+        System.out.println(characterReplacement(s, k));
+    }
+}
