daef02372931a76d94009bad41fe953970b78c36
Anandee Khare <khareanandee11@gmail.com>
2026-03-22 10:21:49 +0530

    day 35: minimum window substring using sliding window

---
+++ java/day_35_min_window_substring.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+
+public class Day35 {
+
+    static String minWindow(String s, String t) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, count = 0, minLen = Integer.MAX_VALUE, start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char c = s.charAt(right);
+
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) >= 0) count++;
+            }
+
+            while (count == t.length()) {
+
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                if (map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) + 1);
+                    if (map.get(leftChar) > 0) count--;
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+
+    public static void main(String[] args) {
+
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+
+        System.out.println(minWindow(s, t));
+    }
+}
