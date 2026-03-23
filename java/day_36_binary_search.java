2bee15eb220ecf0e73328f796fb7f3bb3101c503
Anandee Khare <khareanandee11@gmail.com>
2026-03-23 15:26:28 +0530

    day 36: implemented binary search

---
+++ java/day_36_binary_search.java
@@ -0,0 +1,30 @@
+public class Day36 {
+
+    static int binarySearch(int[] arr, int target) {
+
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6,7};
+        int target = 4;
+
+        System.out.println(binarySearch(arr, target));
+    }
+}
