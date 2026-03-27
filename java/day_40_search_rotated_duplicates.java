bdc213369d6889f1c4635b1644da05237dbf636d
Anandee Khare <khareanandee11@gmail.com>
2026-03-27 07:42:31 +0530

    day 40: search in rotated array with duplicates

---
+++ java/day_40_search_rotated_duplicates.java
@@ -0,0 +1,48 @@
+public class Day40 {
+
+    static boolean search(int[] nums, int target) {
+
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            // duplicates case
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            // left sorted
+            else if (nums[left] <= nums[mid]) {
+
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            } else { // right sorted
+
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,5,6,0,0,1,2};
+        int target = 0;
+
+        System.out.println(search(arr, target));
+    }
+}
