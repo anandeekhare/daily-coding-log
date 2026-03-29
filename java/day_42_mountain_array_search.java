2476e5d436503aec473a5ec6d98c92f96a782c55
Anandee Khare <khareanandee11@gmail.com>
2026-03-29 23:32:44 +0530

    day 42: search in mountain array using peak and binary search

---
+++ java/day_42_mountain_array_search.java
@@ -0,0 +1,53 @@
+public class Day42 {
+
+    static int findPeak(int[] arr) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    static int binarySearch(int[] arr, int target, int left, int right, boolean asc) {
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) return mid;
+
+            if (asc) {
+                if (arr[mid] < target) left = mid + 1;
+                else right = mid - 1;
+            } else {
+                if (arr[mid] > target) left = mid + 1;
+                else right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    static int search(int[] arr, int target) {
+
+        int peak = findPeak(arr);
+
+        int left = binarySearch(arr, target, 0, peak, true);
+        if (left != -1) return left;
+
+        return binarySearch(arr, target, peak + 1, arr.length - 1, false);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,3,5,7,6,4,2};
+        int target = 6;
+
+        System.out.println(search(arr, target));
+    }
+}
