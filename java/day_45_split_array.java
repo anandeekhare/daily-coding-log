63de94fc7b6a5902a56cd940b661f72c8f8407aa
Anandee Khare <khareanandee11@gmail.com>
2026-04-02 10:22:29 +0530

    day 45: split array largest sum using binary search on answer

---
+++ java/day_45_split_array.java
@@ -0,0 +1,49 @@
+public class Day45 {
+
+    static int splitArray(int[] nums, int k) {
+
+        int left = 0, right = 0;
+
+        for (int n : nums) {
+            left = Math.max(left, n); // minimum largest sum
+            right += n;               // maximum possible sum
+        }
+
+        int ans = right;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            int subarrays = 1;
+            int currentSum = 0;
+
+            for (int n : nums) {
+
+                if (currentSum + n > mid) {
+                    subarrays++;
+                    currentSum = 0;
+                }
+
+                currentSum += n;
+            }
+
+            if (subarrays <= k) {
+                ans = mid;
+                right = mid - 1; // try smaller max sum
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {7,2,5,10,8};
+        int k = 2;
+
+        System.out.println(splitArray(nums, k));
+    }
+}
