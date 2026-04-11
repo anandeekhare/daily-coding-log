ea65e166f8947a58d7adf29225365e0bc8e5cc95
Anandee Khare <khareanandee11@gmail.com>
2026-04-11 23:31:01 +0530

    day 52: bfs traversal in graph

---
+++ java/day_52_bfs_graph.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Day52 {
+
+    static void bfs(int start, List<List<Integer>> adj, int n) {
+
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+
+            int node = q.poll();
+            System.out.print(node + " ");
+
+            for (int neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(1).add(3);
+        adj.get(2).add(4);
+
+        bfs(0, adj, n);
+    }
+}
